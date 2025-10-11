e73620af2522c3d64fc9903163c749c09e1ce661
Shiva Gupta <gshiva53@gmail.com>
2025-10-11 09:46:48 +0930

    feat: task name element handles name change

--- frontend/src/pages/TaskCard.jsx
+++ frontend/src/pages/TaskCard.jsx
@@ -1,25 +1,30 @@
+import { createSignal, Show } from "solid-js";
+
 const TaskCard = (props) => {
+  const [showTaskNameAsInput, setShowTaskNameAsInput] = createSignal(false);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="w-sm border-2 bg-stone-200 m-2 text-center">
         <p>{props.id}</p>
-        <p>{props.name}</p>
-        <input
-          type="text"
-          onChange={(e) => {
-            props.updateTodoItem(props.id, props.isComplete, e);
-          }}
-          //	[
-          //            props.updateTodoItem,
-          //            props.id,
-          //            // Solid automatically passes the event as the
-          //            // last argument to the handler function.
-          //            // The name can be derived from the event.
-          //            props.isComplete,
-          //          ]}
+        <Show
+          when={showTaskNameAsInput()}
+          fallback={
+            <p onClick={() => setShowTaskNameAsInput(true)}>{props.name}</p>
+          }
         >
-          {props.name}
-        </input>
+          <input
+            className="w-xs m-2"
+            type="text"
+            placeholder={props.name}
+            onChange={(e) => {
+              props.updateTodoItem(props.id, props.isComplete, e);
+              setShowTaskNameAsInput(false);
+            }}
+          >
+            {props.name}
+          </input>
+        </Show>
         <p>{props.isComplete}</p>
         <button
           className="bg-blue-200 text-center p-2 m-2"
